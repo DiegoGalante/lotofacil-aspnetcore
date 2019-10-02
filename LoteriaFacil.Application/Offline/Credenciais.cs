121f34bb1ea21ebe2493c77dbe583d50977cd585
Diego Galante <diegogalantte@gmail.com>
2019-10-02 14:11:34 -0300

    Teste do gitignore antes de upar arquivos desnecessários.

---
+++ LoteriaFacil.Application/Offline/Credenciais.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LoteriaFacil.Application.Offline
+{
+    public static class Credenciais
+    {
+        public readonly static string EMAIL = string.Empty;
+        public readonly static string PASSWORD = string.Empty;
+    }
+}
+
